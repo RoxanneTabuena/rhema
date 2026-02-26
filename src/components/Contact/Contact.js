2edf929a85d0c875b7bbd2d24d3eb40ca4ca8f54
Roxanne Tabuena <roxyannet@gmail.com>
2026-02-26 14:16:11 -0800

    create and add general info component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,3 +1,4 @@
+import { Info } from './Info/Info'
 import style from './contact.module.css'
 export const Contact = () => {
     return (
@@ -5,7 +6,9 @@ export const Contact = () => {
         <h1 className = {style.logo}>Contact Us</h1>
         <div className={style.content}>
             <p className={style.main}>{`"How can we assist you?"`}</p>
+            <Info type="locations"/>
+            <Info type="contact"/>
         </div>
-        {/* office location, contact info, booking selection */}
+        {/* booking selection */} 
     </div>)
 } 
---
+++ src/components/Contact/Info/Info.js
@@ -0,0 +1,55 @@
+import style from './info.module.css'
+export const Info = ({type}) => {
+    let info = {
+        locations: {
+            h3: 'LOCATIONS',
+            content: [
+                {
+                h4: 'Oahu',
+                p: '94-515 Ukee Street #201; Waipahu, HI 96797'
+                },
+                {
+                h4: 'Maui',
+                p: '94-515 Ukee Street #201; Waipahu, HI 96797'
+                },
+            ]
+        },
+        contact: {
+            h3: 'CONTACT',
+            content: [
+                {
+                h4: 'Phone',
+                p: '(808) 677-5700'
+                },
+                {
+                h4: 'Fax',
+                p: '(808) 677-0097'
+                },
+            ]
+        }
+    }
+    info = info[type]
+    return (
+        <div className={style.info}>
+            <h3>{info.h3}</h3>
+            {info.content.map((a)=>{
+                if (a.p.includes(';')){
+                    const b = a.p.split(';')
+                    return (
+                        <div className={style.art} key={a}>
+                            <h4>{a.h4}</h4>
+                            <p>{b[0]}</p>
+                            <p>{b[1]}</p>
+                        </div>
+                    )
+                }
+                return (
+                    <div className={style.art} key={a}>
+                        <h4>{a.h4}</h4>
+                        <p>{a.p}</p>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
