a9d5722b3d3f25091b428bc4530f083c6e18fede
Roxanne Tabuena <roxyannet@gmail.com>
2026-02-26 13:44:11 -0800

    outline content and style

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,5 +3,9 @@ export const Contact = () => {
     return (
     <div className={style.contact}>
         <h1 className = {style.logo}>Contact Us</h1>
+        <div className={style.content}>
+            <p className={style.main}>{`"How can we assist you?"`}</p>
+        </div>
+        {/* office location, contact info, booking selection */}
     </div>)
-}
+} 
