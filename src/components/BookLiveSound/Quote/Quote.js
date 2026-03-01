fb45806dc8973297ea69e43841cfe460e1572baa
Roxanne Tabuena <roxyannet@gmail.com>
2026-03-01 15:32:07 -0800

    psuedocode content

---
+++ src/components/BookLiveSound/Quote/Quote.js
@@ -0,0 +1,12 @@
+import style from './quote.module.css'
+export const Quote = () => {
+    return (
+        <div className={style.quote}>
+            {/* contact: name, phone, email; 
+            event: audience size ,date(s), start time, end time, 
+            venue: name/descrioption, address; 
+            audience size, event descriotion  */}
+            {/* consent, not a robot */}
+        </div>
+    )
+}
