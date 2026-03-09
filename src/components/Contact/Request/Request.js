5797b01b4072542a07a41d5766562aa4f728bd0d
Roxanne Tabuena <roxyannet@gmail.com>
2026-03-09 14:27:52 -0700

    update style

--- src/components/Contact/Request/Request.js
+++ src/components/Contact/Request/Request.js
@@ -3,7 +3,7 @@ import style from './request.module.css'
 export const Request = () => {
     return (
     <div className={style.request}>
-        <h1 className = {style.logo}>Connect</h1>
+        <h1 className = {style.logo}>Rental Request</h1>
         <div className={style.content}>
             <p className={style.main}>how can we assist you?</p>
             <Order/>
