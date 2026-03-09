cb772a9ae9a7ce713030dfb4cb658fa602b81cd1
Roxanne Tabuena <roxyannet@gmail.com>
2026-03-09 14:06:27 -0700

    remove logs

--- src/components/Rentals/Display/Items/Items.js
+++ src/components/Rentals/Display/Items/Items.js
@@ -1,17 +1,15 @@
 import { Preview } from './Preview'
 import style from './items.module.css'
 export const Items = ({items, brand}) => {
-    if(brand){
-        console.log(items)
+    if(brand !== 'all'){
         items= items.filter((i)=>{
                     return i.brand === brand
                 })
             }
-            console.log(items)
     return (
         <div className={style.items}>
             {items.map((i)=>{
-                return <Preview title={i.name} key={i.name}/>
+                return <Preview item={i} key={i.name}/>
             })}
         </div>
     )
