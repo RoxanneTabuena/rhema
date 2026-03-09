e22678b4800a81160c615811a24f63c541f48596
Roxanne Tabuena <roxyannet@gmail.com>
2026-03-09 14:07:48 -0700

    create add item component

---
+++ src/components/Rentals/Display/Items/Add/Add.js
@@ -0,0 +1,19 @@
+import style from './add.module.css'
+import { useContext } from 'react'
+import { CartContext } from '../../../../../context/CartContext'
+
+export const Add = ({item}) => {
+    const [state, dispatch] = useContext(CartContext)
+
+    const add = (item) => {
+        const action = { type: 'ADD_ITEM', payload:item}
+        dispatch(action)
+        console.log(state.items)
+    }
+
+    return (
+        <div onClick={()=>{add(item)}} className={style.add}>
+            add to order
+        </div>
+    )
+}
