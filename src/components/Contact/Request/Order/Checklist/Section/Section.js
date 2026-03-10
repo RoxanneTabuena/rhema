57bef498d3ed74acdd1ece68c6099861927d7b44
Roxanne Tabuena <roxyannet@gmail.com>
2026-03-10 10:40:53 -0700

    display selector when checked

--- src/components/Contact/Request/Order/Checklist/Section/Section.js
+++ src/components/Contact/Request/Order/Checklist/Section/Section.js
@@ -1,15 +1,19 @@
-import { useContext, useState, useEffect } from 'react'
+import { Selector } from './Selector/Selector'
+import { useContext, useState } from 'react'
 import { CartContext } from '../../../../../../context/CartContext'
 import style from './section.module.css'
 export const Section = ({category, title}) => {
     const [state, dispatch] = useContext(CartContext)
     let items = state.items.filter((i)=>{return i.category===title})
     const [open, setOpen] = useState(items.length>0)
-    console.log(open)
+
     return (
         <div className={style.section}>
-            <input>{open? 'X' : '0'}</input>
-            <p>{title}</p>
+            <h4>
+                <input type="checkbox" checked={open} onChange={()=>{setOpen(!open)}}></input>
+                <p>{title}</p>
+            </h4>
+            { open && <Selector/>}
         </div>
     )
 }
---
+++ src/components/Contact/Request/Order/Checklist/Section/Selector/Selector.js
@@ -0,0 +1,8 @@
+import style from './selector.module.css'
+export const Selector = () => {
+    return (
+        <div className={style.selector}>
+            hiya hiya hiya
+        </div>
+    )
+}
