9586203241f9291820aad430e72012830b242ae8
Roxanne Tabuena <roxyannet@gmail.com>
2026-03-10 10:00:58 -0700

    create update qty function

---
+++ src/components/Contact/Request/Order/Current/Itemline/Qty.js
@@ -0,0 +1,31 @@
+import { useContext, useState } from 'react'
+import { CartContext } from '../../../../../../context/CartContext'
+import style from './itemline.module.css'
+export const Qty = ({item}) => {
+    const [edit, setEdit] = useState(false)
+    const [state, dispatch] = useContext(CartContext)
+
+    // update item quantity
+    const update = (q) => {
+        const newItem = {...item, qty:Number(q)}
+        console.log(newItem)
+        const action =  {type: 'UPDATE_QTY', payload:newItem}
+        dispatch(action)
+    }
+
+    // show input on qty click
+    const change =( <input 
+                        type="number" 
+                        value={item.qty} 
+                        onChange={(e)=>{update(e.target.value)}}
+                        onBlur={()=>{setEdit(false)}}></input>)
+
+    // static qty display
+    let view = <p>{item.qty}</p>
+
+    return (
+        <div className={style.qty} onClick={()=>{setEdit(true)}}>
+            {edit? change : view}
+        </div>
+    )
+}
