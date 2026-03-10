ccfc554c8b077fff3e187a1f21c81e9300139892
Roxanne Tabuena <roxyannet@gmail.com>
2026-03-10 10:00:25 -0700

    add buttons to item lines : inc up, inc down, edit qty, and remove item

--- src/components/Contact/Request/Order/Current/Current.js
+++ src/components/Contact/Request/Order/Current/Current.js
@@ -4,13 +4,11 @@ import { CartContext } from '../../../../../context/CartContext'
 import style from './current.module.css'
 export const Current = () => {
     const [state, dispatch] = useContext(CartContext)
-    console.log(state.items)
     return (
         <div className={style.current}>
             <h3>Current Order</h3>
             {state.items.map((i)=>{
-                return <Itemline key={i.id} item={i}/>
-        
+                return <Itemline key={i.id} id={i.id}/>
             })}
         </div>
     )
--- src/components/Contact/Request/Order/Current/Itemline/Itemline.js
+++ src/components/Contact/Request/Order/Current/Itemline/Itemline.js
@@ -1,13 +1,22 @@
+import { useContext } from 'react'
+import { CartContext } from '../../../../../../context/CartContext'
+import { Qty } from './Qty'
+import { Up } from './Up'
+import { Down } from './Down'
+import { Remove } from './Remove'
 import style from './itemline.module.css'
-export const Itemline = ({item}) => {
-    const {name, qty} = item
+export const Itemline = ({id}) => {
+    const [state, dispatch] = useContext(CartContext)
+    const item = state.items.find((i)=>{return i.id === id})
+
     return (
         <div className={style.itemline}>
-            <p>{name}</p>
-            <p>{qty}</p>
-            {/* increase qty */}
-            {/* decrease qty */}
-            {/* remove */}
+            <Remove item={item}/>
+            <p>{item.name}</p>
+            <Down item={item} />
+            <Qty item={item}/>
+            <Up item={item}/>
+            
         </div>
     )
 }
