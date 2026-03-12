59e88a9bc611beb14685dfc49672bbae0bcb97ca
Roxanne Tabuena <roxyannet@gmail.com>
2026-03-11 18:04:27 -0700

    add state

--- src/components/Root/Head/Nav/Menu/Group/Line/Line.js
+++ src/components/Root/Head/Nav/Menu/Group/Line/Line.js
@@ -1,8 +1,9 @@
+import { NavLink } from 'react-router-dom'
 import style from './line.module.css'
-export const Line = () => {
+export const Line = ({title, link}) => {
     return (
         <div className={style.line}>
-
+            <NavLink to={link}>{title}</NavLink>
         </div>
     )
 }
