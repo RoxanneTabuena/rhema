8a075151c4926642ab9cda4e3bd4d93e73f9175f
Roxanne Tabuena <roxyannet@gmail.com>
2026-03-11 18:06:57 -0700

    add state

--- src/components/Root/Head/Nav/Menu/Group/Group.js
+++ src/components/Root/Head/Nav/Menu/Group/Group.js
@@ -1,8 +1,12 @@
+import { Line } from './Line/line'
 import style from './group.module.css'
-export const Group = () => {
+export const Group = ({children}) => {
+
     return (
         <div className={style.group}>
-
+            { children.map((c)=>{
+                return <Line key={c.key} link={c.value} title={c.key}/>
+            })}
         </div>
     )
 }
