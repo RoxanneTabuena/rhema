5568345afd80ea60f3602bb648cac6dc1b84ec46
Roxanne Tabuena <roxyannet@gmail.com>
2026-03-18 14:23:57 -0700

    extract translation function into separate group component

--- src/components/Root/Head/Nav/Menu/Group/Group.js
+++ src/components/Root/Head/Nav/Menu/Group/Group.js
@@ -1,14 +1,21 @@
+import { useState } from 'react'
 import style from './group.module.css'
 import { Line } from './Line/Line'
-export const Group = ( {title, link, kids}   ) => {
-    return (
-        <div className={style.group}>
-            <Line title={title} link={link}/>
-            <div className={style.kids}>
-                {kids.map(([key, node])=>{
-                    return <Line key={key} title={key} link={node.link}/>
-                })}
+export const Group = ([key, value]) => {
+        // extract link and children from object
+        const { link, children } = value
+
+        return (
+            <div key={key} >
+                {/* render title no matter what */}
+                <Line title={key} link={link} children={children}/>
+                {children &&
+                // add group styling when there are children present
+                <div className={style.group}>
+                    {/* run the translator until an endpoint is found */}
+                    {Object.entries(children).map(Group)}
+                </div>
+                }
             </div>
-        </div>
-    )
-}
+        )
+    }
--- src/components/Root/Head/Nav/Menu/Menu.js
+++ src/components/Root/Head/Nav/Menu/Menu.js
@@ -1,32 +1,13 @@
-import { Line } from './Group/Line/Line'
+import { Group } from './Group/Group'
 import { siteMap } from '../sitemap'
 import { Nav } from './Nav'
 import style from './menu.module.css'
 
 export const Menu = () => {
 
-    const translate = ([key, value]) => {
-        // extract link and children from object
-        const { link, children } = value
-
-        return (
-            <div key={key} >
-                {/* render title no matter what */}
-                <Line title={key} link={link} children={children}/>
-                {children &&
-                // add group styling when there are children present
-                <div className={style.group}>
-                    {/* run the translator until an endpoint is found */}
-                    {Object.entries(children).map(translate)}
-                </div>
-                }
-            </div>
-        )
-    }
-
     return (
         <div className={style.menu}>
-            {Object.entries(siteMap).map(translate)}
+            {Object.entries(siteMap).map(Group)}
             <Nav />
         </div>
     )
