3c8722c59cc576d07a05e7605d9c7da48d3cb631
Roxanne Tabuena <roxyannet@gmail.com>
2026-03-24 11:56:30 -0700

    Add Outside click toggle

--- src/components/Root/Head/Peek/Peek.js
+++ src/components/Root/Head/Peek/Peek.js
@@ -1,10 +1,17 @@
 import { Current } from '../../../Contact/Booking/Request/Order/Current/Current'
 import style from './peek.module.css'
 export const Peek = ({close}) => {
+    const viewCheck = (t) => {
+        if(t.className===style.peek){
+            close()
+        }
+    }
     return (
-        <div className={style.peek}>
-            <button onClick={close}x>x</button>
-            <Current/>
+        <div className={style.peek} onClick={(e)=>{viewCheck(e.target)}}>
+            <div className={style.window}>
+                <button onClick={close}x>x</button>
+                <Current/>
+            </div>
         </div>
     )
 }
