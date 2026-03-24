8fb8fab500b5762a3c9cd262e959e1721696c89b
Roxanne Tabuena <roxyannet@gmail.com>
2026-03-24 13:19:03 -0700

    hide counter on empty cart

--- src/components/Root/Head/Peek/Counter.js
+++ src/components/Root/Head/Peek/Counter.js
@@ -1,7 +1,7 @@
 import style from './peek.module.css'
 export const Counter = ({count, tog}) => {
     return (
-        <button className={style.counter} onClick={tog}>
+        <button className={count > 0 ? style.counter: style.hide} onClick={tog}>
             <p className={style.count}>{count}</p>
             peek img
         </button>
