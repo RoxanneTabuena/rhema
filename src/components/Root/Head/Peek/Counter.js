ca3021480a5b028759ad4f1305c1b36b30b2fec0
Roxanne Tabuena <roxyannet@gmail.com>
2026-03-30 09:34:33 -0700

    add cart icon

--- src/components/Root/Head/Peek/Counter.js
+++ src/components/Root/Head/Peek/Counter.js
@@ -3,7 +3,7 @@ export const Counter = ({count, tog}) => {
     return (
         <button className={count > 0 ? style.counter: style.hide} onClick={tog}>
             <p className={style.count}>{count}</p>
-            peek img
+            <img className={style.case} src="https://imagehostclub.s3.us-west-1.amazonaws.com/rhema/roadcase.png" alt="purple road case"></img>
         </button>
     )
 }
