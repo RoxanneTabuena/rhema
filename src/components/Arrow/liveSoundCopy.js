8788e1e37b4beec96486925f6b94e8e3595b4803
Roxanne Tabuena <roxyannet@gmail.com>
2026-04-09 10:10:50 -0700

    switch arrow to navlink

--- src/components/Arrow/liveSoundCopy.js
+++ src/components/Arrow/liveSoundCopy.js
@@ -1,13 +1,17 @@
 export const liveSoundDir = {
     'Full Service Sound' : {
         title: 'Full Service Sound',
-        text: `We provide end-to-end sound solutions for events of any scale. This includes system design, equipment rental, setup, and on-site engineering to ensure clear, balanced audio throughout the venue. We tailor each setup to the specific needs of performers, presenters, and audiences.`
+        text: `We provide end-to-end sound solutions for events of any scale. This includes system design, equipment rental, setup, and on-site engineering to ensure clear, balanced audio throughout the venue. We tailor each setup to the specific needs of performers, presenters, and audiences.`,
+        link: {
+            text: 'Book Now',
+            path: '/bookLiveSound'
+        }
     },
     'Crew' : {
         title: 'Audio Experts',
         text: `Our experienced engineers and techs can bring reliable sound to any environment. We are people focused, and ready to collaborate with you to deliver seamless, professional results.`,
         link: {
-            text: 'meet the team',
+            text: 'Meet The Team',
             path: '/ourExperts'
         }
     },
@@ -15,7 +19,7 @@ export const liveSoundDir = {
         title: 'State of the Art Equipment',
         text: `L'acoustics is the industry leader in premium sound reinforcement technologies. Its the best sound quality on the market and can only be experienced on island through Rhema.`,
         link: {
-            text: 'learn more',
+            text: 'Learn More',
             path: `https://www.l-acoustics.com/about-us/discover-l-acoustics/`
         }
     }
--- src/components/LiveSound/Roller/Roller.js
+++ src/components/LiveSound/Roller/Roller.js
@@ -1,5 +1,5 @@
 // import { useState, useEffect } from 'react'
-import { Arrow } from '../../Arrow/Arrow'
+import { NavLink } from 'react-router-dom'
 import style from './roller.module.css'
 
 export const Roller = ({ title, text, link, index, total }) => {
@@ -8,7 +8,7 @@ export const Roller = ({ title, text, link, index, total }) => {
             <h3>{title}</h3>
             <div className={style.art}>
                 <p>{text}</p>
-                {link && <Arrow text={link.text} path={link.path} />}
+                {link && <NavLink to={link.path} className={style.link}>{link.text}</NavLink>}
             </div>
         </div>
     )
