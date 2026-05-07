2f227773992cfad57e7b53ccff47bb5d14e43931
Roxanne Tabuena <roxyannet@gmail.com>
2026-05-07 11:13:08 -0700

    update to new title/summary format

--- src/components/Contact/Booking/Request/Request.js
+++ src/components/Contact/Booking/Request/Request.js
@@ -1,12 +1,10 @@
+import { Summary } from '../../../Blocks/Summary/Summary'
 import { Order } from './Order/Order'
 import style from './request.module.css'
 export const Request = () => {
     return (
     <div className={style.request}>
-        <h1 className = {style.logo}>Rental Request</h1>
-        <div className={style.content}>
-            <p className={style.main}>how can we assist you?</p>
-            <Order/>
-        </div>
+        <Summary summary="Reserve your gear here!"/>
+        <Order/>
     </div>)
 } 
