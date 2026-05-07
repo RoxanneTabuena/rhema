b3849a9eeee53b20de54d91acaf6c8e85ce6e971
Roxanne Tabuena <roxyannet@gmail.com>
2026-05-07 10:32:49 -0700

    style for mobile and desktop

--- src/components/Rentals/Display/Items/Product.js
+++ src/components/Rentals/Display/Items/Product.js
@@ -1,16 +1,41 @@
+import { NavLink } from 'react-router-dom'
 import { Add } from './Add/Add'
-import style from './items.module.css'
+import style from './product.module.css'
 export const Product = ({item}) => {
     return (
         <div className={style.product}>
-            <h2>{item.name}</h2>
-            <Add item={item}/>
-            {/* product title */}
-            {/* product picture */}
-            {/* product description */}
-            {/* product availability schedule */}
-            {/* product factory link */}
-            {/* product spec sheet */}
+            <div className={style.desk}>
+                <img src="https://imagehostclub.s3.us-west-1.amazonaws.com/rhema/picPlaceholder.png" className={style.prodImg} alt="product pic placeholder"></img>
+                <div>
+                    <h3>{item.name}</h3>
+                    <p className={style.description}>
+                        One sentence product description.  
+                        <NavLink href="#" className={style.factory}>factory info</NavLink>
+                    <ul className={style.specs}>
+                        <li>product spec</li>
+                        <li>product spec</li>
+                        <li>product spec</li>
+                    </ul>
+                        <Add item={item}/>
+                    </p>
+                </div>
+            </div>
+            <div className={style.mobile}>
+                <h3>{item.name}</h3>
+                <div className={style.glance}>
+                    <img src="https://imagehostclub.s3.us-west-1.amazonaws.com/rhema/picPlaceholder.png" className={style.prodImg} alt="product pic placeholder"></img>
+                    <ul className={style.specs}>
+                        <li>product spec</li>
+                        <li>product spec</li>
+                        <li>product spec</li>
+                    </ul>
+                </div>
+                <p className={style.description}>
+                    One sentence product description.  
+                    <NavLink href="#" className={style.factory}>factory info</NavLink>
+                </p>
+                <Add item={item}/>
+            </div>
         </div>
     )
 }
