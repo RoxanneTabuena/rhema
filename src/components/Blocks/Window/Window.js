d68330f378dfcdf29a79a41ef9fbe9ffdf6518bd
Roxanne Tabuena <roxyannet@gmail.com>
2026-05-08 12:00:59 -0700

    integrate context

--- src/components/Blocks/Window/Window.js
+++ src/components/Blocks/Window/Window.js
@@ -1,11 +1,20 @@
-
+import { useContext } from 'react'
+import { WindowContext } from '../../../context/WindowContext'
 
 import style from './window.module.css'
 export const Window = ({content}) => {
+    // get window state
+    const [windowState, windowDispatch] = useContext(WindowContext)
+    // package close action
+    const closeWindow = () => {
+        const action = { type: 'CLOSE'}
+        windowDispatch(action)
+        console.log(windowState.windowOpen)
+    }
     return (
         <div className={style.windowCont}>
             <div className={style.window}>
-                <button onClick={'CLOSE WINDOW'}x>x</button>
+                <button onClick={closeWindow}x>x</button>
                 <div>
                     <p>{content}</p>
                 </div>
